2d0cb207d969f3e2db29e600100a52e66c784728
Jonathan Miller <230051081+jmiller-moko@users.noreply.github.com>
2025-12-16 15:27:14 -0600

    Reorganization

---
+++ src/templates/moko-cassiopeia/custom.php
@@ -0,0 +1,35 @@
+<?php
+/* Copyright (C) 2025 Moko Consulting <hello@mokoconsulting.tech>
+
+ This file is part of a Moko Consulting project.
+
+ SPDX-License-Identifier: GPL-3.0-or-later
+
+ This program is free software; you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/ .
+
+ # FILE INFORMATION
+ DEFGROUP: Joomla.Template.Site
+ INGROUP: Moko-Cassiopeia
+ REPO: https://github.com/mokoconsulting-tech/moko-cassiopeia
+ PATH: ./templates/moko-cassiopeia/custom.php
+ VERSION: 03.00.00
+ BRIEF: Custom entry template file for Moko-Cassiopeia with user-defined overrides
+ */
+
+	function console_log($output, $with_script_tags = true) {
+		$js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
+	');';
+		if ($with_script_tags) {
+			$js_code = '<script>' . $js_code . '</script>';
+		}
+		echo $js_code;
+	}
+?>
+<!--
+	Custom code included here
+-->
