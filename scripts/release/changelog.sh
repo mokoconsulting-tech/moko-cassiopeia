8f38ed8b4df0fa9ab77a8ce77bf346181a0d3c4a
Jonathan Miller <230051081+jmiller-moko@users.noreply.github.com>
2025-12-26 23:45:25 -0600

    `LICENSE.md` to `LICENSE`

--- scripts/release/changelog.sh
+++ scripts/release/changelog.sh
@@ -16,7 +16,7 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License (./LICENSE.md).
+# You should have received a copy of the GNU General Public License (./LICENSE).
 # -----------------------------------------------------------------------------
 # FILE INFORMATION
 # DEFGROUP: MokoStandards
