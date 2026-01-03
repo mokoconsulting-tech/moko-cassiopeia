d57bf419d51b7f59c5bb98c5d0b04390a4f6104f
Jonathan Miller <jmiller2979@gmail.com>
2026-01-03 15:16:36 -0600

    Remove copyright comments from changelog.sh
    Removed copyright and licensing information from the changelog script.
    
    Signed-off-by: Jonathan Miller <jmiller2979@gmail.com>

--- scripts/validate/changelog.sh
+++ scripts/validate/changelog.sh
@@ -1,33 +1,3 @@
-# ============================================================================
-# Copyright (C) 2025 Moko Consulting <hello@mokoconsulting.tech>
-#
-# This file is part of a Moko Consulting project.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# FILE INFORMATION
-# DEFGROUP: Scripts.Validate
-# INGROUP: MokoStandards.Release
-# REPO: https://github.com/mokoconsulting-tech/MokoStandards
-# PATH: /scripts/validate/changelog.sh
-# VERSION: 01.00.00
-# BRIEF: Validates CHANGELOG.md structure and confirms a release section exists for the current version inferred from branch, tag, or env.
-# NOTE:
-# ============================================================================
-
 set -euo pipefail
 
 json_escape() {
