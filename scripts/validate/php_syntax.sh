beba3809722ed3a8e415585836606ca5ce0e3f02
Jonathan Miller <jmiller2979@gmail.com>
2026-01-03 15:18:57 -0600

    Clean up php_syntax.sh header comments
    Removed copyright and licensing information from the script.
    
    Signed-off-by: Jonathan Miller <jmiller2979@gmail.com>

--- scripts/validate/php_syntax.sh
+++ scripts/validate/php_syntax.sh
@@ -1,33 +1,4 @@
-# ============================================================================
-# Copyright (C) 2025 Moko Consulting <hello@mokoconsulting.tech>
-#
-# This file is part of a Moko Consulting project.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# FILE INFORMATION
-# DEFGROUP: Scripts.Validate
-# INGROUP: MokoStandards.Release
-# REPO: https://github.com/mokoconsulting-tech/MokoStandards
-# PATH: /scripts/validate/php_syntax.sh
-# VERSION: 01.00.00
-# BRIEF: Runs PHP lint over all PHP files under src. If PHP is unavailable, returns ok with a warning payload.
-# NOTE:
-# ============================================================================
-
+#!/usr/bin/env bash
 set -euo pipefail
 
 SRC_DIR="${SRC_DIR:-src}"
