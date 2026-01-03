53a2205204a132da4adb9a2409adea967a8b0491
Jonathan Miller <jmiller2979@gmail.com>
2026-01-03 15:16:55 -0600

    Remove copyright notice from language_structure.sh
    Removed copyright and licensing information from the script.
    
    Signed-off-by: Jonathan Miller <jmiller2979@gmail.com>

--- scripts/validate/language_structure.sh
+++ scripts/validate/language_structure.sh
@@ -1,33 +1,3 @@
-# ============================================================================
-# Copyright (C) 2025 Moko Consulting <hello@mokoconsulting.tech>
-#
-# This file is part of a Moko Consulting project.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# FILE INFORMATION
-# DEFGROUP: Scripts.Validate
-# INGROUP: MokoStandards.Release
-# REPO: https://github.com/mokoconsulting-tech/MokoStandards
-# PATH: /scripts/validate/language_structure.sh
-# VERSION: 01.00.00
-# BRIEF: Validates Joomla language structure under src/language and enforces folder and INI naming conventions.
-# NOTE:
-# ============================================================================
-
 set -euo pipefail
 
 SRC_DIR="${SRC_DIR:-src}"
